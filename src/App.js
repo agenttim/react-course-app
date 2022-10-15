55dae7940b02dc65bedbd04f109ebc74efd44ce8
agent_tim <agent_tim@mail.ru>
2022-10-15 08:04:36 +0500

    2022.10.15_1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import CostItem from "./components/CostItem";
+import CostItem from "./components/Costs/CostItem";
+import Costs from "./components/Costs/Costs";
 
 
 function App() {
+
+    const costs = [
+        {
+            date: new Date(2021, 2, 12),
+            description: "Холодильник",
+            amount: 999.99
+        },
+        {
+            date: new Date(2021, 11, 15),
+            description: "Ноутбук",
+            amount: 1999.21
+        },
+        {
+            date: new Date(2021, 3, 1),
+            description: "Джинсы",
+            amount: 59.00
+        },
+    ];
+
   return (
-      <div>
-          <h1>Начнем изучение React!</h1>
-          <CostItem></CostItem>
-      </div>
+      <Costs
+          costs={costs}
+      />
+
   );
 }
 
--- src/components/CostItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function CostItem() {
-    return (
-        <h2>Элемент расхода</h2>
-    );
-}
-
-export default CostItem;
